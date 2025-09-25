603bccf5a510ff1292c4b30c876ee304f41e07f0
IT24103055 <IT24103055@my.sliit.lk>
2025-09-26 00:17:57 +0530

    begin

---
+++ src/main/java/org/votingsystem/voting_system_for_award_nominations/VotingSystemForAwardNominationsApplication.java
@@ -0,0 +1,13 @@
+package org.votingsystem.voting_system_for_award_nominations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSystemForAwardNominationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingSystemForAwardNominationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/votingsystem/voting_system_for_award_nominations/VotingSystemForAwardNominationsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.votingsystem.voting_system_for_award_nominations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSystemForAwardNominationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
